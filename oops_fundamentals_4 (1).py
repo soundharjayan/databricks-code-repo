b296af2cdc881e2c6feb91bda0b1fb345140b146
Soundhar Jayan <soundharjayan@gmail.com>
2025-11-28 20:47:19 +0530

    Update oops_fundamentals_4 (1).py

--- oops_fundamentals_4 (1).py
+++ oops_fundamentals_4 (1).py
@@ -14,7 +14,7 @@
 
 #class is a main program that holds the subprograms
 #xls class (template/blueprint) holds subprograms (functions) (tabs)
-print("hello team")
+print("hello team in Git")
 class xls:
     def tab1(self):
         pass
